d616be33f66a380ee46d576e337eb10ee47a273e
Kevin Klues <klueska@gmail.com>
2008-02-25 22:21:53 +0000

    Update to add Alarm stuff

--- mammark.sh
+++ mammark.sh
@@ -2,9 +2,7 @@
 #After this is set up, run 'make telosc threads' to compile with thread support
 
 #Replace with your absolute path to the root of mammark
-if [ "z$MAMMARK_DIR" = "z" ] ; then
-    MAMMARK_DIR="/home/cire/mm_t2/t2_mm3"
-    TOSMAKE_PATH="$TOSMAKE_PATH $MAMMARK_DIR/support/make"
-fi
+MAMMARK_DIR="/Users/klueska/sensornets/tos/mammark"
+TOSMAKE_PATH="$TOSMAKE_PATH $MAMMARK_DIR/support/make"
 export MAMMARK_DIR
 export TOSMAKE_PATH
