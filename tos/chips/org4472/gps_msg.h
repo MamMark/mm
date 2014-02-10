4980782a353740116bb21b17b209ca74c123f838
Eric B. Decker <cire831@gmail.com>
2014-02-10 11:55:05 -0700

    org4472: switch to decimal time

--- tos/chips/org4472/gps_msg.h
+++ tos/chips/org4472/gps_msg.h
@@ -1,8 +1,12 @@
 /*
+ * Copyright (c) 2008, 2014 Eric B. Decker
+ *
  * GPS MSG defines
  *
  * @author Eric B. Decker (cire831@gmail.com)
  * @date 24 Aug 2008
+ *
+ * Updated 08 Feb 2014: switch to decimal time.
  *
  * Defines for the Msg machine. Such as how long to look for
  * a fast gps fix.
@@ -21,10 +25,10 @@
  * over rides all.
  *
  * short window, 20 secs.
- * Long window,  5 mins (5 mins * 60 sec/min * 1024 tics/sec)
+ * Long window,  5 mins (5 mins * 60 sec/min * 1000 tics/sec)
  */
 
-#define GPS_MSG_SHORT_WINDOW (20*1024UL)
-#define GPS_MSG_LONG_WINDOW (5*60*1024UL)
+#define GPS_MSG_SHORT_WINDOW (20*1000UL)
+#define GPS_MSG_LONG_WINDOW  (5*60*1000UL)
 
 #endif /* __GPS_MSG_H__ */
