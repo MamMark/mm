6f0fd46559fbc8e1c1fd726dc326462d0ebd3287
Eric B. Decker <cire831@gmail.com>
2014-09-15 21:31:24 -0700

    docs: update to reflect new build environment

--- mammark.sh
+++
@@ -1,8 +0,0 @@
-#Set up environment variable to point to location of thread implementation
-#After this is set up, run 'make telosc threads' to compile with thread support
-
-#Replace with your absolute path to the root of mammark
-MM_ROOT="/Users/klueska/sensornets/tos/mammark"
-TOSMAKE_PATH="$TOSMAKE_PATH $MM_ROOT/support/make"
-export MM_ROOT
-export TOSMAKE_PATH
