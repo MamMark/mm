59b2e6e5387b43421c6cfc798d6044c4bc3aea94
Eric B. Decker <cire831@gmail.com>
2016-01-15 17:34:12 -0800

    mm5a: add si446x hardware files

---
+++ tos/platforms/mm5a/hardware/si446x/RadioConfig.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) 2016, Eric B. Decker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ * - Neither the name of the copyright holder nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eric B. Decker
+ */
+
+#ifndef __RADIOCONFIG_H__
+#define __RADIOCONFIG_H__
+
+#include <Timer.h>
+#include <message.h>
+
+/*
+ * Also include the platform dependent si446x definitions generated
+ *  from the SiLabs WDS program (Wireless Development Studio.
+ */
+#include "radio_config_si446x.h"
+#include "radio_platform_si446x.h"
+
+/* TODO: need to figure out correct power value */
+#ifndef SI446X_DEF_RFPOWER
+#define SI446X_DEF_RFPOWER	31
+#endif
+
+/*
+ * channel to use for FREQCTRL
+ *
+ * Freq is set by h/w at 433MHz, single channel.
+ */
+#ifndef SI446X_DEF_CHANNEL
+#define SI446X_DEF_CHANNEL	0
+#endif
+
+/* The number of microseconds a sending mote will wait for an acknowledgement */
+#ifndef SOFTWAREACK_TIMEOUT
+#define SOFTWAREACK_TIMEOUT	800
+#endif
+
+/**
+ * This is the timer type of the radio alarm interface
+ */
+typedef TMicro   TRadio;
+typedef uint16_t tradio_size;
+
+/**
+ * The number of radio alarm ticks per one microsecond .
+ *
+ * The main exp5438 clock is at 8MHz (decimal), TA0 is used to
+ * provide TMicro (DCO -> MCLK -> SMCLK -> /8 -> TA0).
+ *
+ * The SFD pin is on pin 8.1 which is connected to TA0.CCI1B.
+ * TA0 is run off the TMicro so we use TA0 for the capture and also
+ * runs the TMicro clocks.
+ */
+#define RADIO_ALARM_MICROSEC    1
+
+
+/**
+ * The base two logarithm of the number of radio alarm ticks per one millisecond
+ *
+ * Originally TinyOS defined time in terms of powers of two.  And 
+ * RADIO_ALARM_MILLI_EXP expressing millisecs (actually mis, binary millisecs)
+ * isn't a problem.
+ *
+ * 2**10 = 1024
+ */
+#define RADIO_ALARM_MILLI_EXP	10
+
+
+
+/**
+ * Make PACKET_LINK automaticaly enabled for Ieee154MessageC
+ */
+#if !defined(TFRAMES_ENABLED) && !defined(PACKET_LINK)
+#define PACKET_LINK
+#endif
+
+#endif          //__RADIOCONFIG_H__
---
+++ tos/platforms/mm5a/hardware/si446x/Si446xPinsP.nc
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2016 Eric B. Decker
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the
+ *   distribution.
+ *
+ * - Neither the name of the copyright holders nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @author Eric B. Decker <cire831@gmail.com>
+ */
+
+#include "mmPortRegs.h"
+
+module Si446xPinsP {
+  provides {
+    interface Si446xInterface as HW;
+  }
+  uses {
+    interface GpioInterrupt  as RadioNIRQ;
+  }
+}
+implementation {
+  async command uint8_t HW.si446x_cts()             { return SI446X_CTS_P; }
+  async command uint8_t HW.si446x_irqn()            { return SI446X_IRQN_P; }
+  async command uint8_t HW.si446x_sdn()             { return SI446X_SDN_IN; }
+  async command uint8_t HW.si446x_csn()             { return SI446X_CSN_IN; }
+  async command void    HW.si446x_shutdown()        { SI446X_SDN = 1; }
+  async command void    HW.si446x_unshutdown()      { SI446X_SDN = 0; }
+  async command void    HW.si446x_set_cs()          { SI446X_CSN = 0; }
+  async command void    HW.si446x_clr_cs()          { SI446X_CSN = 1; }
+  async command void    HW.si446x_set_low_tx_pwr()  { }
+  async command void    HW.si446x_set_high_tx_pwr() { }
+
+  async command void    HW.si446x_enableInterrupt() {
+    atomic {
+      call RadioNIRQ.enableFallingEdge();
+    }
+  }
+
+  async command void    HW.si446x_disableInterrupt() {
+    atomic {
+      call RadioNIRQ.disable();
+    }
+  }
+
+  async event void RadioNIRQ.fired() {
+    signal HW.si446x_interrupt();
+  }
+}
