dec81399fd3350958ac83d3fc2a258755d1c7fce
Eric B. Decker <cire831@gmail.com>
2016-03-19 18:57:06 -0700

    exp5438_sd: base files based on exp5438_gps

---
+++ tos/platforms/exp5438_sd/platform_panic.h
@@ -0,0 +1,39 @@
+/*
+ * panic codes.
+ */
+
+
+#ifndef __PLATFORM_PANIC_H__
+#define __PLATFORM_PANIC_H__
+
+#include "panic.h"
+
+/*
+ * KERN:	core kernal (defined in panic.h)
+ * ADC:		Analog Digital Conversion subsystem (AdcP.nc)
+ * MISC:
+ * COMM:	communications subsystem
+ * MS:		Mass Storage (FileSystemP, SD)
+ * SS:		Stream Storage, hard fail
+ * SS_RECOV:	Stream Storage, recoverable
+ * GPS:		gps subsystem
+ */
+
+enum {
+#ifdef notdef
+  PANIC_ADC,
+  PANIC_MISC,
+  PANIC_COMM,
+  PANIC_MS,
+  PANIC_SS,
+  PANIC_SS_RECOV,
+#endif
+  PANIC_GPS = PANIC_HC_START,		/* 0x10, see panic.h */
+
+#ifdef notdef
+  PANIC_SNS,
+  PANIC_PWR,
+#endif
+};
+
+#endif /* __PLATFORM_PANIC_H__ */
---
+++ tos/platforms/exp5438_sd/platform_trace.h
@@ -0,0 +1,73 @@
+/**
+ *
+ * Copyright 2015 (c) Eric B. Decker
+ * All rights reserved.
+ *
+ * @author Eric B. Decker
+ */
+
+#ifndef __PLATFORM_TRACE_H__
+#define __PLATFORM_TRACE_H__
+
+#define TRACE_SIZE 64
+
+typedef enum {
+  T_REQ			= 1,
+  T_GRANT		= 2,
+  T_REL			= 3,
+  T_SSR			= 4,
+  T_SSW			= 5,
+  T_GPS			= 6,
+  T_THREAD_STOP		= 7,
+  T_THREAD_START	= 8,
+  T_THREAD_SUSPEND	= 9,
+  T_THREAD_WAKE		= 10,
+
+  T_GPS_DO_GRANT	= 11,
+  T_GPS_DO_DEFERRED	= 12,
+  T_GPS_RELEASING	= 13,
+  T_GPS_RELEASED	= 14,
+  T_GPS_DO_REQUESTED	= 15,
+  T_GPS_HOLD_TIME	= 16,
+  T_SSW_DELAY_TIME	= 17,
+  T_SSW_BLK_TIME	= 18,
+  T_SSW_GRP_TIME	= 19,
+
+  T_RS                  = 20,
+
+  T_R_SFD_UP            = 32,
+  T_R_SFD_DOWN,
+  T_R_SFD_OVW,
+  T_R_SFD_FILL,
+  T_R_SFD_DRAIN,
+  T_R_SFD_FLUSH,
+
+//  T_R_EXCEP,
+//  T_R_EXCEP_1,
+//  T_R_TX_FD,
+//  T_R_TX_FD_1,
+  T_R_TX_PKT,
+//  T_R_RX_FD,
+  T_R_RX_PKT,
+  T_R_RX_RECV,
+  T_R_RX_BAD_CRC,
+//  T_R_RX_OVR,
+//  T_R_RX_OVR_1,
+//  T_R_RX_LOOP,
+//  T_R_RECOVER,
+
+  T_TL                  =64,
+  T_INT_OVR,
+  T_INT_T0A0,
+  T_INT_T0A1,
+  T_INT_P1,
+
+  /*
+   * For debugging Arbiter 1
+   */
+  T_A1_REQ		= 1 + 256,
+  T_A1_GRANT		= 2 + 256,
+  T_A1_REL		= 3 + 256,
+} trace_where_t;
+
+#endif	// __PLATFORM_TRACE_H__
