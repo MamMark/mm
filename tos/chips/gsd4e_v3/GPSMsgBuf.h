f318d26bff08b5ba45340bde38ff4e0f5b6a7e75
Eric B. Decker <cire831@gmail.com>
2017-04-15 22:47:12 -0700

    gps: update GPSMsgBuf.h for new data structures for new algorithms

--- tos/chips/gsd4e_v3/GPSMsgBuf.h
+++ tos/chips/gsd4e_v3/GPSMsgBuf.h
@@ -10,52 +10,47 @@
 #define __GPSMSGBUF_H__
 
 
-#define GPS_MAX_BUF           1024
-#define GPS_MAX_MSG_TABLE     20
+#define GPS_BUF_SIZE 1024
+#define GPS_MAX_MSGS 32
 
 typedef enum {
-  CHECK_OFF = 0,
-  CHECK_NMEA,
-  CHECK_SIRFBIN
-} gps_checksum_t;
+  GPS_MSG_EMPTY = 0,            /* not being used, available */
+  GPS_MSG_FREE,                 /* points at free buffer space */
+  GPS_MSG_FILLING,              /* currently being filled in */
+  GPS_MSG_FULL,                 /* holds a message */
+  GPS_MSG_BUSY,                 /* busy, message is being processed */
+} gms_t;                        /* gps msg state */
 
 
-typedef enum {
-  BC_IDLE = 0,
-  BC_BODY,
-  BC_FLUSHING,
-} buf_collect_state_t;
+typedef struct {
+  uint8_t *data;
+  uint16_t len;
+  uint16_t extra;
+  gms_t    state;
+} gps_msg_t;
 
 
-typedef struct gps_buf_struct {
-  uint16_t                    i_current;
-  uint16_t                    i_begin;
-  uint16_t                    i_limit;
-  uint16_t                    i_checksum;
-  uint16_t                    checksum;
-  gps_checksum_t              checking;
-  uint8_t                     data[GPS_MAX_BUF] __attribute__ ((aligned (2)));
-  buf_collect_state_t         collect_state;
-} gps_buf_t;
-//
-
-typedef enum {
-  MSG_FREE = 0,
-  MSG_IN_USE,
-  MSG_BUSY
-} gps_msg_state_t;
-
-typedef struct gps_msg_struct {
-  uint16_t                    len;
-  uint16_t                    dt_type;
-  uint8_t                     buf[];
-} gps_msg_t __attribute__ ((aligned (1)));
-
-typedef struct gps_msg_table_struct {
-  gps_msg_t                  *msg;
-  uint16_t                    len;
-  gps_msg_state_t             state;
-} gps_msg_table_t;
+/*
+ * Because of strict ordering, both msg slots as well as memory blocks
+ * we can have at most 3 seperate regions, 2 free and 1 full of contiguous
+ * message data.
+ *
+ * So we have free_f, free_t, and head.
+ */
+typedef struct {
+  uint16_t free_f;              /* index of free entry */
+  uint16_t free_t;              /* tail free */
+  uint16_t head;                /* head index of msg queue */
+  uint16_t tail;                /* tail index of msg queue */
+  uint16_t full;                /* number full */
+  uint16_t max_full;            /* how deep did it get */
+} gmc_t;                        /* gps msg control */
 
 
-#endif GPSMSGBUF
+#define MSG_INDEX_INVALID(x) ((x) & 0x8000)
+#define MSG_INDEX_VALID(x)   (((x) & 0x8000) == 0)
+#define MSG_NO_INDEX         (0xffff)
+
+#define MSG_NEXT_INDEX(x) (((x) + 1) & 0x1f)
+
+#endif  /* __GPSMSGBUF_H__ */
