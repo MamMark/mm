8a89af921f781428c1b4f189db5b556474a2ad72
Daniel J Maltbie <dmaltbie@daloma.org>
2017-06-28 22:50:21 -0700

    Tagnet: move TLV related definitions into TagnetTLV.h

--- tos/comm/Tagnet.h
+++ tos/comm/Tagnet.h
@@ -37,8 +37,6 @@
 #ifndef __TAGNET_H__
 #define __TAGNET_H__
 
-#include "TagnetTLV.h"
-
 typedef enum {
   TN_POLL                = 0,
   TN_BEACON              = 1,
@@ -74,16 +72,7 @@ typedef struct tagnet_payload_meta_t {
   uint8_t     this;
 } tagnet_payload_meta_t;
 
-/* structure used to hold configuration values for each of the elements
- * in the tagnet named data tree
- */
-typedef struct TN_data_t {
-  int         id;
-  char*       name_tlv;
-  char*       help_tlv;
-  char*       uq;
-} TN_data_t;
-
+// unique ids used for wiring to generic modules
 #define UQ_TN_ROOT              "UQ_TN_ROOT"
 #define UQ_TN_TAG               "UQ_TN_TAG"
 #define UQ_TN_POLL              "UQ_TN_POLL"
@@ -100,45 +89,5 @@ typedef struct TN_data_t {
 #define UQ_TN_INFO_SENS_GPS_POS "UQ_TN_INFO_SENS_GPS_POS"
 
 #define UQ_TAGNET_ADAPTER_LIST  "UQ_TAGNET_ADAPTER_LIST"
-
-// index into tn_named_data_descriptors
-enum {
-  TN_ROOT_ID,
-  TN_TAG_ID,
-
-  TN_POLL_ID,
-  TN_POLL_NID_ID,
-  TN_POLL_EV_ID,
-  TN_POLL_CNT_ID,
-
-  TN_INFO_ID,
-  TN_INFO_NID_ID,
-  TN_INFO_SENS_ID,
-  TN_INFO_SENS_GPS_ID,
-  TN_INFO_SENS_GPS_POS_ID,
-
-  TN_LAST_ID
-};
-
-/*
- * Index list above must be kept in sync with the row order
- * in this table
- */
-
-const TN_data_t tn_name_data_descriptors[TN_LAST_ID]={
-  {TN_ROOT_ID,"\000\000","\001\009root help",UQ_TN_ROOT},
-  {TN_TAG_ID,TN_TAG_TLV,"\001\008tag help",UQ_TN_TAG},
-
-  {TN_POLL_ID,"\001\004poll","\001\009poll help",UQ_TN_POLL},
-  {TN_POLL_NID_ID,TN_BCAST_NID_TLV,"\1\13poll_nid help",UQ_TN_POLL_NID},
-  {TN_POLL_EV_ID,"\001\002ev","\001\012poll_ev help",UQ_TN_POLL_EV},
-  {TN_POLL_CNT_ID,"\001\003cnt","\001\013poll_cnt help",UQ_TN_POLL_CNT},
-
-  {TN_INFO_ID,"\001\004info","\001\009info help",UQ_TN_INFO},
-  {TN_INFO_NID_ID,TN_BCAST_NID_TLV,"\1\13info_nid help",UQ_TN_INFO_NID},
-  {TN_INFO_SENS_ID,"\001\004sens","\001\011sensor help",UQ_TN_INFO_SENS},
-  {TN_INFO_SENS_GPS_ID,"\001\003gps","\001\013sens_gps help",UQ_TN_INFO_SENS_GPS},
-  {TN_INFO_SENS_GPS_POS_ID,"\001\003pos","\001\017sens_gps_pos help",UQ_TN_INFO_SENS_GPS_POS},
-};
 
 #endif          /* __TAGNET_H__ */
--- tos/comm/TagnetTLV.h
+++ tos/comm/TagnetTLV.h
@@ -63,10 +63,81 @@ typedef struct tagnet_tlv_t {
 
 // 'standard' TLVs
 #define TN_NONE_TLV          "\000\000"
-#define TN_BCAST_NID_TLV     "\005\006\255\255\255\255\255\255"
-#define TN_MY_NID_TLV        "\005\006\042\042\042\042\042\042"
+#define TN_BCAST_NID_TLV     "\005\006\xff\xff\xff\xff\xff\xff"
+#define TN_MY_NID_TLV        "\005\006\x42\x42\x42\x42\x42\x42"
 #define TN_TAG_TLV           "\001\003tag"
 
 #define SIZEOF_TLV(t) (t->len + sizeof(tagnet_tlv_t))
+
+
+// index into tn_named_data_descriptors
+typedef enum {
+  TN_ROOT_ID,
+  TN_TAG_ID,
+
+  TN_POLL_ID,
+  TN_POLL_NID_ID,
+  TN_POLL_EV_ID,
+  TN_POLL_CNT_ID,
+
+  TN_INFO_ID,
+  TN_INFO_NID_ID,
+  TN_INFO_SENS_ID,
+  TN_INFO_SENS_GPS_ID,
+  TN_INFO_SENS_GPS_POS_ID,
+
+  TN_LAST_ID
+} tn_ids_t;
+
+/* structure used to hold configuration values for each of the elements
+ * in the tagnet named data tree
+ */
+typedef struct TN_data_t {
+  tn_ids_t    id;
+  char*       name_tlv;
+  char*       help_tlv;
+  char*       uq;
+} TN_data_t;
+
+/*
+ * Indexed using tn_ids_t from above.
+ *
+ * Ids must be kept in sync with the row index in this table.
+ */
+
+const TN_data_t tn_name_data_descriptors[TN_LAST_ID]={
+  {TN_ROOT_ID,"\000\000","\001\009root help",UQ_TN_ROOT},
+  {TN_TAG_ID,TN_TAG_TLV,"\001\008tag help",UQ_TN_TAG},
+
+  {TN_POLL_ID,"\001\004poll","\001\009poll help",UQ_TN_POLL},
+  {TN_POLL_NID_ID,TN_BCAST_NID_TLV,"\1\13poll_nid help",UQ_TN_POLL_NID},
+  {TN_POLL_EV_ID,"\001\002ev","\001\012poll_ev help",UQ_TN_POLL_EV},
+  {TN_POLL_CNT_ID,"\001\003cnt","\001\013poll_cnt help",UQ_TN_POLL_CNT},
+
+  {TN_INFO_ID,"\001\004info","\001\009info help",UQ_TN_INFO},
+  {TN_INFO_NID_ID,TN_BCAST_NID_TLV,"\1\13info_nid help",UQ_TN_INFO_NID},
+  {TN_INFO_SENS_ID,"\001\004sens","\001\011sensor help",UQ_TN_INFO_SENS},
+  {TN_INFO_SENS_GPS_ID,"\001\003gps","\001\013sens_gps help",UQ_TN_INFO_SENS_GPS},
+  {TN_INFO_SENS_GPS_POS_ID,"\001\003pos","\001\017sens_gps_pos help",UQ_TN_INFO_SENS_GPS_POS},
+};
+
+/*
+ * Tagnet name parsing trace array
+ */
+#define TN_TRACE_PARSE_ARRAY_SIZE  64
+
+typedef struct tagnet_trace_parse {
+  tn_ids_t     id;
+  uint8_t      loc;
+} tagnet_trace_parse_t;
+
+tagnet_trace_parse_t  tn_trace_array[TN_TRACE_PARSE_ARRAY_SIZE];
+uint32_t              tn_trace_index;
+
+void tn_trace_rec(tn_ids_t id, uint8_t loc) {
+  tn_trace_array[tn_trace_index].id = id;
+  tn_trace_array[tn_trace_index].loc = loc;
+  if (tn_trace_index < TN_TRACE_PARSE_ARRAY_SIZE) tn_trace_index++;
+}
 
 #endif
