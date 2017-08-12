17cf9399f92642349847113e433aaa21697097fb
Eric B. Decker <cire831@gmail.com>
2017-08-11 17:36:18 -0700

    move script and associated file for gdb set up
    move from tos/mm to tos/mm/00_99

--- tos/mm/00_gdb/gdb_gettting_started.sh
+++ tos/mm/00_gdb/gdb_gettting_started.sh
@@ -4,9 +4,9 @@ if [[ ! -v MM_ROOT ]]; then
     echo need MM_ROOT defined
     exit 1
 fi
-cp ${MM_ROOT}/tos/mm/gdbinit                    .gdbinit
-cp ${MM_ROOT}/tos/mm/gdb_msp432                 .gdb_msp432
-cp ${MM_ROOT}/tos/mm/gdb_mm                     .gdb_mm
+cp ${MM_ROOT}/tos/mm/00_gdb/gdbinit             .gdbinit
+cp ${MM_ROOT}/tos/mm/00_gdb/gdb_msp432          .gdb_msp432
+cp ${MM_ROOT}/tos/mm/00_gdb/gdb_mm              .gdb_mm
 cp ${MM_ROOT}/tos/platforms/mm6a/gdb_mm6a       .gdb_mm6a
 cp ${MM_ROOT}/tos/platforms/dev6a/gdb_dev6a     .gdb_dev6a
 cp ${MM_ROOT}/tos/chips/gsd4e_v4/gdb_gps        .gdb_gps
