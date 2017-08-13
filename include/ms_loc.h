a81ef8869be31bd6552cff76ed447e4e276d160e
Eric B. Decker <cire831@gmail.com>
2017-08-12 22:22:28 -0700

    update boot sector documentation

--- include/ms_loc.h
+++ include/ms_loc.h
@@ -1,22 +1,34 @@
 /*
  * ms_loc.h - mass storage locator information
+ * Copyright 2017, Eric B. Decker
  * Copyright 2006, 2010 Eric B. Decker, Carl W. Davis
  * Mam-Mark Project
  *
- * Mass storage has well known sectors that contains information about
- * where contiguous files live for use by the mammark tag.
+ * Mass storage is implemented using a SD card, between 2GB and 32GB.
+ *
+ * The tag uses a very simple file system where a fixed number of files are
+ * implemented.  Each file consists of a contiguous number of disk blocks.
+ *
+ * To assist with moving data off the Tag SD, each file is also contained
+ * with in a legit MSDOS Vfat file.  This enables pulling data off the SD
+ * easily once the image is mounted on a typical computer system.
+ *
+ * Areas on the SD managed by the FileSystem include areas for Panic,
+ * Configuration, a Data Stream, and Image storage.
+ *
+ * The FileSystem determines where the different File areas live by
+ * accessing the master directory.  This information lives on sector
+ * 0 of the SD which is the Master Boot Record.  We use an unused block
+ * of space in the middle of the sector.
  *
  * MBR, sector 0, has a dblk locator buried that defines all
- * contiguous file areas (panic, config, and data blocks).
- *
- * PANIC0, sector 2, is a special sector used by panic to locate the
- * panic section of the disk.
+ * contiguous file areas (panic, config, data, and image blocks).
  *
  * It is assumed that the SD has been formated as a super block, using
  * mkdosfs -F32 -I -n2G -vvv /dev/sdb.
  *
  * Sector 0 is the mbr, boot sector.  Sector 1 is unknown but if
- * modified make it so the media won't automount on Linux boxes.  The
+ * modified makes it so the media won't automount on Linux boxes.  The
  * backup boot block is at sector 6.  There are a total of 32 reserved
  * sectors at the front of the file system (0-31, 0 is mbr, etc).
  *
@@ -25,10 +37,15 @@
  * much more efficient then dealing with byte swapping.  This means
  * one can't use nx types for these datums which makes sharing the
  * header files easier too.
+ *
+ * We try as much as possible to make multibyte data have an alignment that
+ * allows for efficient access.  That is we want 16 bit quantities
+ * (half-words) aligned on 16 bit boundaries and 32 bit quantities
+ * (long-words) aligned on 32 bit boundaries.
  */
 
-#ifndef _MS_LOC_H
-#define _MS_LOC_H
+#ifndef __MS_LOC_H__
+#define __MS_LOC_H__
 
 #define DBLK_SECTOR 0
 
@@ -82,4 +99,4 @@ typedef struct {
 } panic0_hdr_t;
 
 
-#endif /* _MS_LOC_H */
+#endif /* __MS_LOC_H__ */
