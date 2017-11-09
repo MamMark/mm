1760bba65fa7855e635458d8a427261bbf3cd00a
Eric B. Decker <cire831@gmail.com>
2017-11-08 17:56:59 -0800

    dumpsd: major refactor.  From explicit to object driven printer

--- tools/utils/dumpsd/dumpsd.py
+++ tools/utils/dumpsd/dumpsd.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+
 import os
 import sys
 import binascii
 import struct
+
+from collections import OrderedDict
 
 
 ####
@@ -25,46 +29,217 @@ LOGICAL_SECTOR_SIZE = 512
 LOGICAL_BLOCK_SIZE  = 508  # excludes trailer
 LOGICAL_HEADER_SIZE = 4
 
-# the dt_descriptor_array contains contains details about each record type,
-# including: name of record, id of record, struct format for unpacking,
-# and format string for printing
+
+class atom(object):
+    '''
+    takes 2-tuple: ('struct_string', 'default_print_format')
+
+    set will set the instance.attribute "val" to the value
+    of the atom's decode of the buffer.
+    '''
+    def __init__(self, a_tuple):
+        self.s_str = a_tuple[0]
+        self.s_rec = struct.Struct(self.s_str)
+        self.p_str = a_tuple[1]
+
+    def __len__(self):
+        return self.s_rec.size
+
+    def __repr__(self):
+        return self.p_str.format(self.val)
+
+    def set(self, buf):
+        '''
+        set the atom.val to the unpack from the format string.
+
+        return the number of bytes (size) consumed
+        '''
+        self.val = self.s_rec.unpack(buf[:self.s_rec.size])[0]
+        return self.s_rec.size
+
+
+class aggie(OrderedDict):
+    '''
+    aggie: aggregation node.
+    takes one parameter a dictionary of key -> {atom | aggie}
+    '''
+    def __init__(self, a_dict):
+        super(aggie, self).__init__(a_dict)
+
+    def __len__(self):
+        l = 0
+        for key, v_obj in self.iteritems():
+            if isinstance(v_obj, atom) or isinstance(v_obj, aggie):
+                l += v_obj.__len__()
+        return l
+
+    def __repr__(self):
+        s = ''
+        for key, v_obj in self.iteritems():
+            if len(s) != 0:
+                s += ', '
+            if isinstance(v_obj, atom):
+                s += key + ': ' + v_obj.__repr__()
+            elif isinstance(v_obj, aggie):
+                s += '[' + key + ': ' + v_obj.__repr__() + ']'
+            else:
+                s += "oops"
+        return s
+
+    def set(self, buf):
+        '''
+        '''
+        consumed = 0
+        for key, v_obj in self.iteritems():
+            consumed += v_obj.set(buf[consumed:])
+        return consumed
+
+
+hdr_len  = atom(('H', '{}'))
+hdr_type = atom(('H', '{}'))
+hdr_xt   = atom(('I', '0x{:04x}'))
+
+hdr_obj    = aggie(OrderedDict([('len', hdr_len), ('type', hdr_type)]))
+hdr_xt_obj = aggie(OrderedDict([('len', hdr_len), ('type', hdr_type), ('xt', hdr_xt)]))
+
+
+def print_hdr_xt(obj):
+    rtype = obj['hdr']['type'].val
+    print('{:08x} ({:2}) {:6} -- '.format(
+        obj['hdr']['xt'].val,
+        rtype, dt_records[rtype][2])),
+
+
+# EVENT
+
+event_names = {
+     1: "SURFACED",
+     2: "SUBMERGED",
+     3: "DOCKED",
+     4: "UNDOCKED",
+     5: "GPS_BOOT",
+     6: "GPS_BOOT_TIME",
+     7: "GPS_RECONFIG",
+     8: "GPS_START",
+     9: "GPS_OFF",
+    10: "GPS_FAST",
+    11: "GPS_FIRST",
+    12: "GPS_SATS_2",
+    13: "GPS_SATS_7",
+    14: "GPS_SATS_29",
+    15: "GPS_CYCLE_TIME",
+    16: "GPS_GEO",
+    17: "GPS_XYZ",
+    18: "GPS_TIME",
+    19: "GPS_RX_ERR",
+    20: "SSW_DELAY_TIME",
+    21: "SSW_BLK_TIME",
+    22: "SSW_GRP_TIME",
+}
+
+event_arg   = atom(('I', '0x{:04x}'))
+event_event = atom(('H', '{}'))
+event_obj   = aggie(OrderedDict([('hdr', hdr_xt_obj),
+                                 ('arg0', event_arg),
+                                 ('arg1', event_arg),
+                                 ('arg2', event_arg),
+                                 ('arg3', event_arg),
+                                 ('event', event_event)]))
+
+
+def decode_tintryalf(buf, obj):
+    pass
+
+def decode_reboot(buf, obj):
+    pass
+
+def decode_version(buf, obj):
+    pass
+
+def decode_sync(buf, obj):
+    pass
+
+def decode_panic(buf, obj):
+    pass
+
+def decode_flush(buf, obj):
+    pass
+
+
+def decode_event(buf, event_obj):
+    print('=============')
+    event_obj.set(buf)
+    print(event_obj)
+    print_hdr_xt(event_obj)
+    event = event_obj['event'].val
+    print('({:2}) {:20}  {}  {}  {}  {}'.format(
+        event, event_names[event],
+        event_obj['arg0'].val,
+        event_obj['arg1'].val,
+        event_obj['arg2'].val,
+        event_obj['arg3'].val))
+    print('=============')
+
+
+def decode_debug(buf, obj):
+    pass
+
+def decode_gps_version(buf, obj):
+    pass
+
+def decode_gps_time(buf, obj):
+    pass
+
+def decode_gps_geo(buf, obj):
+    pass
+
+def decode_gps_xyz(buf, obj):
+    pass
+
+def decode_sensor_data(buf, obj):
+    pass
+
+def decode_sensor_set(buf, obj):
+    pass
+
+def decode_test(buf, obj):
+    pass
+
+def decode_note(buf, obj):
+    pass
+
+def decode_config(buf, obj):
+    pass
+
+def decode_gps_raw_sirfbin(buf, obj):
+    pass
+
+
+# dt_records
 #
-dt_descriptor_array = [
-    ("DT_TINTRYALF", 0,
-     "HH", "length:{} type:{}"),
-    ("DT_CONFIG", 1,
-     "HH", "length:{} type:{}"),
-    ("DT_SYNC", 2,
-     "HHIII", "length:{} type:{}, timestamp:{}, majik:{}, cycle:{}"),
-    ("DT_REBOOT", 3,
-     "HHIIIIIQIIIIH", "length:{} type:{}, timestamp:{}, majik:{}, cycle:{}, reset{}, reboot:{}, elapsed:{}, strange:{}, vector_chk:{}, image_chk:{}, reason:{}"),
-    ("DT_PANIC", 4,
-     "HHLIIIIBBB", "length:{} type:{}, timestamp:{3}, arg0:{}, arg1:{}, arg2:{}, arg3:{}, pcode:{}, where:{}, index:{}"),
-    ("DT_VERSION", 5,
-     "HHBBHBB", "length:{} type:{}, major:{}, minor:{}, build:{}, rev:{}, model:{}"),
-    ("DT_EVENT", 6,
-     "HHIIIIH", "length:{} type:{}, arg0:0x{:02x}, arg1:0x{:02x}, arg2:0x{:02x}, arg3:0x{:02x}, event:{}"),
-    ("DT_DEBUG", 7,
-     "HH", "length:{} type:{}"),
-    ("DT_GPS_VERSION", 8,
-     "HH", "length:{} type:{}"),
-    ("DT_GPS_TIME", 9,
-     "HH", "length:{} type:{}"),
-    ("DT_GPS_GEO", 10,
-     "HH", "length:{} type:{}"),
-    ("DT_GPS_XYZ", 11,
-     "HH", "length:{} type:{}"),
-    ("DT_SENSOR_DATA", 12,
-     "HHIIH", "length:{} type:{}, timestamp:{}, schedule:{}, sns_id:{}"),
-    ("DT_SENSOR_SET", 13,
-     "HHIIHH", "length:{} type:{}, timestamp:{}, schedule:{}, mask:{}, mask_id:{}"),
-    ("DT_TEST", 14,
-     "HH", "length:{} type:{}"),
-    ("DT_NOTE", 15,
-     "HHHHBBBBB", "length:{} type:{}, note_len:{}, {}.{}.{} {}:{}:{}"),
-    ("DT_GPS_RAW_SIRFBIN", 16,
-     "HHIIB", "length:{} type:{}, timestamp:{}, mark:{}, chip:{}"),
-  ]
+# dictionary of all data_typed records we understand
+# dict key is the record id.
+#
+dt_records = {
+     0: (decode_tintryalf,          None,           "TINTRYALF"),
+     1: (decode_reboot,             None,           "REBOOT"),
+     2: (decode_version,            None,           "VERSION"),
+     3: (decode_sync,               None,           "SYNC"),
+     4: (decode_panic,              None,           "PANIC"),
+     5: (decode_flush,              None,           "FLUSH"),
+     6: (decode_event,              event_obj,      "EVENT"),
+     7: (decode_debug,              None,           "DEBUG"),
+    16: (decode_gps_version,        None,           "GPS_VERSION"),
+    17: (decode_gps_time,           None,           "GPS_TIME"),
+    18: (decode_gps_geo,            None,           "GPS_GEO"),
+    19: (decode_gps_xyz,            None,           "GPS_XYZ"),
+    20: (decode_sensor_data,        None,           "SENSOR_DATA"),
+    21: (decode_sensor_set,         None,           "SENSOR_SET"),
+    22: (decode_test,               None,           "TEST"),
+    23: (decode_note,               None,           "NOTE"),
+    24: (decode_config,             None,           "CONFIG"),
+    32: (decode_gps_raw_sirfbin,    None,           "GPS_RAW"),
+  }
 
 
 # gen_data_bytes generates data bytes from file (stripped of non-data bytes).
@@ -126,11 +301,7 @@ def gen_records(fd):
         if (not hdr) or (len(hdr) < LOGICAL_HEADER_SIZE):
             break
         rlen, rtyp = dt_hdr.unpack(hdr)
-        if (rtyp > len(dt_descriptor_array)):
-            break
-        if (rtyp != dt_descriptor_array[rtyp][1]):
-            break
-        if (rtyp == dt_descriptor_array[0][1]): # tinytryalf
+        if (rtyp == 0): # tinytryalf
             data_bytes.send(-1)   # skip to next sector
             continue
         # return record header fields plus record contents
@@ -161,17 +332,20 @@ def insert_space(st):
 def main(source):
     total = 0
     with open(source, 'rb') as fd:
-        for rlen, rtyp, offset, buf in gen_records(fd):
-            print(rtyp, rlen, len(buf), (fd.tell()-512)+offset, hex((fd.tell()-512)+offset), insert_space(buf))
-            if (rlen < len(buf)) or (rtyp >= len(dt_descriptor_array)):
-                break
-            dtd = dt_descriptor_array[rtyp]
-            dt_rec = struct.Struct(dtd[2])
-            dt_info = dt_rec.unpack(buf[:dt_rec.size])
-            print(dtd[0])
-            print(dtd[3].format(*dt_info))
+        for rlen, rtype, offset, buf in gen_records(fd):
+            print("type:{}, len:{}, next:{}({}), buf:{}".format(
+                rtype, rlen, (fd.tell()-512)+offset,
+                hex((fd.tell()-512)+offset),
+                insert_space(buf)))
+            if (rtype in dt_records):
+                decode = dt_records[rtype][0]
+                obj    = dt_records[rtype][1]
+                decode(buf, obj)
+            else:
+                print('unknown (0x{:02x})'.format(rtype))
             total += rlen
             print('----')
+
         print(fd.tell(),  total)
 
 
