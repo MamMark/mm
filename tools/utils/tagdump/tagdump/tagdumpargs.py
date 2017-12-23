ffedab428d8ef1de35ca7f4eba98fc30ad00915b
Eric B. Decker <cire831@gmail.com>
2017-12-23 03:41:43 -0800

    tagdump: args, nuke sectors, add start/last rec_num, verbose 0
    o add auto_int for using hex for starting position
    o add auto_upper to force rtypes to upper case
    o nuke --first_sector and --last_sector
    o add  --start_rec and --last_rec
    o set default verbosity to 0

--- tools/utils/tagdump/tagdump/tagdumpargs.py
+++ tools/utils/tagdump/tagdump/tagdumpargs.py
@@ -1,39 +1,59 @@
 from __init__ import __version__ as VERSION
 import argparse
+
+
+def auto_int(x):
+    return int(x, 0)
+
+
+def auto_upper(x):
+    return x.upper()
+
+
 def parseargs():
     parser = argparse.ArgumentParser(
         description='Pretty print content of Tag Data logfile')
+
     parser.add_argument('input',
                         type=argparse.FileType('rb'),
                         help='input file')
+
     parser.add_argument('-V', '--version',
                         action='version',
                         version='%(prog)s ' + VERSION)
+
     parser.add_argument('--rtypes',
-                        type=str,
+                        type=auto_upper,
                         help='output records matching types in list')
-    parser.add_argument('-f', '--first_sector',
-                        type=int,
-                        help='begin with START_SECTOR')
-    parser.add_argument('-l', '--last_sector',
-                        type=int,
-                        help='sector to stop with.')
+
     parser.add_argument('-j', '--jump',
-                        type=int,
+                        type=auto_int,
                         help='set input file position')
+
     # not working yet
     parser.add_argument('-s', '--start_time',
                         type=int,
                         help='include records with datetime greater than START_TIME')
+
     # not working yet
     parser.add_argument('-e', '--end_time',
                         type=int,
                         help='stop with records after END_TIME')
+
+    parser.add_argument('-r', '--start_rec',
+                        type=int,
+                        help='starting record to dump.')
+
+    parser.add_argument('-l', '--last_rec',
+                        type=int,
+                        help='last record to dump.')
+
     # 0v print record details, suppress recoverable errors
     # v  also print the record header and all errors
     # vv also print the record buffer
-    parser.add_argument('-v', '--verbosity',
+    parser.add_argument('-v', '--verbose',
                         action='count',
-                        default=1,
+                        default=0,
                         help='increase output verbosity')
+
     return parser.parse_args()
