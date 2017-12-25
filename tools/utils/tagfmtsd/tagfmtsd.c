e69d3d0bce7243536e58fc4a73d11d4daa524a94
Eric B. Decker <cire831@gmail.com>
2017-12-25 05:38:17 -0800

    dblk: update tagfmtsd to create dblk_dir
    o incept date

--- tools/utils/tagfmtsd/tagfmtsd.c
+++ tools/utils/tagfmtsd/tagfmtsd.c
@@ -15,17 +15,20 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
+#include <time.h>
 
 #include <mm_byteswap.h>
 #include <fat_fs.h>
 #include <fatxfc.h>
 #include <ms.h>
 
+#include <dblk_dir.h>
+
 
 extern fs_loc_t loc;
 
 
-#define VERSION "tagfmtsd: v4.0.0  2017/10/19\n"
+#define VERSION "tagfmtsd: v4.1.0  2017/12/24\n"
 
 int debug	= 0,
     verbose	= 0,
@@ -75,9 +78,12 @@ static void usage(char *name) {
 
 
 void
-display_info(void) {
+display_info(uint8_t *buf) {
     fat_dir_entry_t *de;
     u32_t rds;
+    dblk_dir_t *ddp;
+    uint32_t    sum, *p32;
+    int err, i;
 
     fprintf(stderr, "fs_loc:  p:   s: %-8x   e: %x\n",
             loc.locators[FS_LOC_PANIC].start,
@@ -119,8 +125,92 @@ display_info(void) {
 	rds = (CF_LE_16(de->starthi) << 16) | CF_LE_16(de->start);
 	fprintf(stderr, "DBLK0001:  start  0x%04x  size: %10u (0x%x)\n",
 		fx_clu2sec(rds), CF_LE_32(de->size), CF_LE_32(de->size));
+        /*
+         * examine the dblk dir
+         */
+        err = ms_read_blk(loc.locators[FS_LOC_DBLK].start, buf);
+        if (err)
+          fprintf(stderr, "could not read dblk dir: %s (0x%x)\n",
+                  fx_dsp_err(err), err);
+        else {
+          fprintf(stderr, "  DBLK_DIR: checksum ");
+          p32 = (void *) buf;
+          for (i = 0; i < DBLK_DIR_QUADS; i++)
+            sum += *p32++;
+          if (sum) fprintf(stderr, "bad, ");
+          else     fprintf(stderr, "good, ");
+          ddp = (void *) buf;
+          fprintf(stderr, "sigs: %08x %08x\n",
+                  ddp->dblk_dir_sig, ddp->dblk_dir_sig_a);
+          fprintf(stderr, "  low: %u (0x%04x), high: %u (0x%04x)\n",
+                  ddp->dblk_low, ddp->dblk_low,
+                  ddp->dblk_high, ddp->dblk_high);
+          fprintf(stderr, "  incept: %04d/%02d/%02d-%02d:%02d:%02d (%d) GMT\n",
+                  ddp->incept_date.yr,  ddp->incept_date.mon,
+                  ddp->incept_date.day, ddp->incept_date.hr,
+                  ddp->incept_date.min, ddp->incept_date.sec,
+                  ddp->incept_date.dow);
+        }
     } else
 	fprintf(stderr, "DBLK0001: not found\n");
+
+}
+
+
+
+/*
+ * write_dblk_dir: create and write out the dblk directory.
+ *
+ * Create the directory.  Includes:
+ * o start and end (low/high)
+ * o incept date in datetime format.  (creation date)
+ *
+ * write directory out to first sector of the dblk area.
+ */
+int write_dblk_dir(uint8_t *buf) {
+  dblk_dir_t *ddp;
+  struct tm   split_gmtime, *gmp;
+  time_t      time_secs;
+  uint32_t    sum, *p32;
+  int err, i;
+
+  gmp = &split_gmtime;
+  time_secs = time(NULL);
+  if (gmtime_r(&time_secs, gmp) == NULL)
+    fprintf(stderr, "*** write_dblk_dir: gmtime_r failed.\n");
+
+  /*
+   * modify what is returned to make it conform to what dateTime
+   * expects.  TI RTC format.
+   */
+  if (gmp->tm_sec > 59)                 /* check for leap sec */
+    gmp->tm_sec = 59;                   /* ignore it */
+  gmp->tm_mon++;
+  gmp->tm_year += 1900;                 /* adjust to current century */
+
+  memset(buf, 0, MS_BUF_SIZE);
+  ddp = (void *) buf;
+  ddp->dblk_dir_sig     = DBLK_DIR_SIG;
+  ddp->dblk_low         = loc.locators[FS_LOC_DBLK].start;
+  ddp->dblk_high        = loc.locators[FS_LOC_DBLK].end;
+  ddp->incept_date.yr   = gmp->tm_year;
+  ddp->incept_date.mon  = gmp->tm_mon;
+  ddp->incept_date.day  = gmp->tm_mday;
+  ddp->incept_date.hr   = gmp->tm_hour;
+  ddp->incept_date.min  = gmp->tm_min;
+  ddp->incept_date.sec  = gmp->tm_sec;
+  ddp->incept_date.dow  = gmp->tm_wday;
+  ddp->dblk_dir_sig_a   = DBLK_DIR_SIG;
+  ddp->chksum           = 0;
+  p32 = (void *) buf;
+  for (i = 0; i < DBLK_DIR_QUADS; i++)
+    sum += *p32++;
+  ddp->chksum = (uint32_t) (0 - sum);
+  err = ms_write_blk(loc.locators[FS_LOC_DBLK].start, buf);
+  if (err)
+    fprintf(stderr, "write_dblk_dir: DBLK0001: could not write dblk dir: %s (0x%x)\n",
+            fx_dsp_err(err), err);
+  return err;
 }
 
 
@@ -193,7 +283,7 @@ int main(int argc, char **argv) {
 	exit(1);
     }
     if (!do_write) {
-	display_info();
+	display_info(buf);
 	exit(0);
     }
     do_fs_loc = 1;
@@ -250,6 +340,9 @@ int main(int argc, char **argv) {
     if (err) {
 	fprintf(stderr, "fx_create_contig: DBLK0001: %s (0x%x)\n", fx_dsp_err(err), err);
 	do_fs_loc = 0;
+    } else {
+      if (write_dblk_dir(buf))
+        do_fs_loc = 0;
     }
     if (do_fs_loc) {
       fprintf(stderr, "*** writing fs locator\n");
@@ -260,7 +353,7 @@ int main(int argc, char **argv) {
       }
     }
 
-    display_info();
+    display_info(buf);
 
     if (!do_fs_loc)
       fprintf(stderr, "*** no changes written\n");
