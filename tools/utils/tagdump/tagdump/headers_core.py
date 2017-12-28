1f5196963634de8c896c5849bfd0e64064331eca
Eric B. Decker <cire831@gmail.com>
2017-12-27 17:10:59 -0800

    tagdump: force header_core to use "<" little endian order
    this avoids and local host native alignment restrictions.
    We found that Q (2quad) alignment could be forced to a 2quad
    boundary.  On the 32 bit ARM this is only forced to a quad
    boundary.

--- tools/utils/tagdump/tagdump/headers_core.py
+++ tools/utils/tagdump/tagdump/headers_core.py
@@ -15,43 +15,43 @@ DT_H_REVISION           = 0x00000008
 # do not include the pad byte.  Each hdr definition handles
 # the pad byte differently.
 
-dt_hdr_str    = 'HHIQH'
+dt_hdr_str    = '<HHIQH'
 dt_hdr_struct = struct.Struct(dt_hdr_str)
 dt_hdr_size   = dt_hdr_struct.size
 dt_sync_majik = 0xdedf00ef
-quad_struct   = struct.Struct('I')      # for searching for syncs
+quad_struct   = struct.Struct('<I')      # for searching for syncs
 
 DT_REBOOT     = 1
 DT_SYNC       = 3
 
 dt_hdr_obj = aggie(OrderedDict([
-    ('len',     atom(('H', '{}'))),
-    ('type',    atom(('H', '{}'))),
-    ('recnum',  atom(('I', '{}'))),
-    ('st',      atom(('Q', '0x{:x}'))),
-    ('recsum',  atom(('H', '0x{:04x}')))]))
+    ('len',     atom(('<H', '{}'))),
+    ('type',    atom(('<H', '{}'))),
+    ('recnum',  atom(('<I', '{}'))),
+    ('st',      atom(('<Q', '0x{:x}'))),
+    ('recsum',  atom(('<H', '0x{:04x}')))]))
 
 datetime_obj = aggie(OrderedDict([
-    ('jiffies', atom(('H', '{}'))),
-    ('yr',      atom(('H', '{}'))),
-    ('mon',     atom(('B', '{}'))),
-    ('day',     atom(('B', '{}'))),
-    ('hr',      atom(('B', '{}'))),
-    ('min',     atom(('B', '{}'))),
-    ('sec',     atom(('B', '{}'))),
-    ('dow',     atom(('B', '{}')))]))
+    ('jiffies', atom(('<H', '{}'))),
+    ('yr',      atom(('<H', '{}'))),
+    ('mon',     atom(('<B', '{}'))),
+    ('day',     atom(('<B', '{}'))),
+    ('hr',      atom(('<B', '{}'))),
+    ('min',     atom(('<B', '{}'))),
+    ('sec',     atom(('<B', '{}'))),
+    ('dow',     atom(('<B', '{}')))]))
 
 dt_simple_hdr   = aggie(OrderedDict([('hdr', dt_hdr_obj)]))
 
 dt_reboot_obj   = aggie(OrderedDict([
     ('hdr',     dt_hdr_obj),
-    ('pad0',    atom(('H', '{:04x}'))),
-    ('majik',   atom(('I', '{:08x}'))),
-    ('prev',    atom(('I', '{:08x}'))),
-    ('dt_rev',  atom(('I', '{:08x}'))),
+    ('pad0',    atom(('<H', '{:04x}'))),
+    ('majik',   atom(('<I', '{:08x}'))),
+    ('prev',    atom(('<I', '{:08x}'))),
+    ('dt_rev',  atom(('<I', '{:08x}'))),
     ('datetime',atom(('10s', '{}', binascii.hexlify))),
-    ('pad1',    atom(('H',  '{}'))),
-    ('pad2',    atom(('I',  '{}')))]))
+    ('pad1',    atom(('<H',  '{}'))),
+    ('pad2',    atom(('<I',  '{}')))]))
 
 #
 # reboot is followed by the ow_control_block
@@ -59,50 +59,50 @@ dt_reboot_obj   = aggie(OrderedDict([
 # see OverWatch/overwatch.h.
 #
 owcb_obj        = aggie(OrderedDict([
-    ('ow_sig',          atom(('I', '0x{:08x}'))),
-    ('rpt',             atom(('I', '0x{:08x}'))),
-    ('st',              atom(('Q', '0x{:08x}'))),
-    ('reset_status',    atom(('I', '0x{:08x}'))),
-    ('reset_others',    atom(('I', '0x{:08x}'))),
-    ('from_base',       atom(('I', '0x{:08x}'))),
-    ('reboot_count',    atom(('I', '{}'))),
-    ('ow_req',          atom(('B', '{}'))),
-    ('reboot_reason',   atom(('B', '{}'))),
-    ('ow_boot_mode',    atom(('B', '{}'))),
-    ('owt_action',      atom(('B', '{}'))),
-    ('ow_sig_b',        atom(('I', '0x{:08x}'))),
-    ('strange',         atom(('I', '{}'))),
-    ('strange_loc',     atom(('I', '0x{:04x}'))),
-    ('vec_chk_fail',    atom(('I', '{}'))),
-    ('image_chk_fail',  atom(('I', '{}'))),
-    ('elapsed',         atom(('Q', '0x{:08x}'))),
-    ('ow_sig_c',        atom(('I', '0x{:08x}')))
+    ('ow_sig',          atom(('<I', '0x{:08x}'))),
+    ('rpt',             atom(('<I', '0x{:08x}'))),
+    ('st',              atom(('<Q', '0x{:08x}'))),
+    ('reset_status',    atom(('<I', '0x{:08x}'))),
+    ('reset_others',    atom(('<I', '0x{:08x}'))),
+    ('from_base',       atom(('<I', '0x{:08x}'))),
+    ('reboot_count',    atom(('<I', '{}'))),
+    ('ow_req',          atom(('<B', '{}'))),
+    ('reboot_reason',   atom(('<B', '{}'))),
+    ('ow_boot_mode',    atom(('<B', '{}'))),
+    ('owt_action',      atom(('<B', '{}'))),
+    ('ow_sig_b',        atom(('<I', '0x{:08x}'))),
+    ('strange',         atom(('<I', '{}'))),
+    ('strange_loc',     atom(('<I', '0x{:04x}'))),
+    ('vec_chk_fail',    atom(('<I', '{}'))),
+    ('image_chk_fail',  atom(('<I', '{}'))),
+    ('elapsed',         atom(('<Q', '0x{:08x}'))),
+    ('ow_sig_c',        atom(('<I', '0x{:08x}')))
 ]))
 
 
 dt_version_obj  = aggie(OrderedDict([
     ('hdr',       dt_hdr_obj),
-    ('pad',       atom(('H', '{:04x}'))),
-    ('base',      atom(('I', '{:08x}')))]))
+    ('pad',       atom(('<H', '{:04x}'))),
+    ('base',      atom(('<I', '{:08x}')))]))
 
 
 hw_version_obj      = aggie(OrderedDict([
-    ('rev',       atom(('B', '{:x}'))),
-    ('model',     atom(('B', '{:x}')))]))
+    ('rev',       atom(('<B', '{:x}'))),
+    ('model',     atom(('<B', '{:x}')))]))
 
 
 image_version_obj   = aggie(OrderedDict([
-    ('build',     atom(('H', '{:x}'))),
-    ('minor',     atom(('B', '{:x}'))),
-    ('major',     atom(('B', '{:x}')))]))
+    ('build',     atom(('<H', '{:x}'))),
+    ('minor',     atom(('<B', '{:x}'))),
+    ('major',     atom(('<B', '{:x}')))]))
 
 
 image_info_obj  = aggie(OrderedDict([
-    ('ii_sig',    atom(('I', '0x{:08x}'))),
-    ('im_start',  atom(('I', '0x{:08x}'))),
-    ('im_len',    atom(('I', '0x{:08x}'))),
-    ('vect_chk',  atom(('I', '0x{:08x}'))),
-    ('im_chk',    atom(('I', '0x{:08x}'))),
+    ('ii_sig',    atom(('<I', '0x{:08x}'))),
+    ('im_start',  atom(('<I', '0x{:08x}'))),
+    ('im_len',    atom(('<I', '0x{:08x}'))),
+    ('vect_chk',  atom(('<I', '0x{:08x}'))),
+    ('im_chk',    atom(('<I', '0x{:08x}'))),
     ('ver_id',    image_version_obj),
     ('desc0',     atom(('44s', '0x{:x}'))),
     ('desc1',     atom(('44s', '0x{:x}'))),
@@ -112,9 +112,9 @@ image_info_obj  = aggie(OrderedDict([
 
 dt_sync_obj     = aggie(OrderedDict([
     ('hdr',       dt_hdr_obj),
-    ('pad0',      atom(('H', '{:04x}'))),
-    ('majik',     atom(('I', '{:08x}'))),
-    ('prev_sync', atom(('I', '{:x}'))),
+    ('pad0',      atom(('<H', '{:04x}'))),
+    ('majik',     atom(('<I', '{:08x}'))),
+    ('prev_sync', atom(('<I', '{:x}'))),
     ('datetime',  atom(('10s','{}', binascii.hexlify)))]))
 
 
@@ -149,12 +149,12 @@ event_names = {
 
 dt_event_obj    = aggie(OrderedDict([
     ('hdr',   dt_hdr_obj),
-    ('event', atom(('H', '{}'))),
-    ('arg0',  atom(('I', '0x{:04x}'))),
-    ('arg1',  atom(('I', '0x{:04x}'))),
-    ('arg2',  atom(('I', '0x{:04x}'))),
-    ('arg3',  atom(('I', '0x{:04x}'))),
-    ('pcode', atom(('B', '{}'))),
-    ('w',     atom(('B', '{}')))]))
+    ('event', atom(('<H', '{}'))),
+    ('arg0',  atom(('<I', '0x{:04x}'))),
+    ('arg1',  atom(('<I', '0x{:04x}'))),
+    ('arg2',  atom(('<I', '0x{:04x}'))),
+    ('arg3',  atom(('<I', '0x{:04x}'))),
+    ('pcode', atom(('<B', '{}'))),
+    ('w',     atom(('<B', '{}')))]))
 
 dt_debug_obj    = dt_simple_hdr
