b03582172b010bd3a3375bb857784615d8ea8956
Eric B. Decker <cire831@gmail.com>
2017-12-28 04:24:06 -0800

    tagdump: make dump_buf take a prefix string

--- tools/utils/tagdump/tagdump/tagdump.py
+++ tools/utils/tagdump/tagdump/tagdump.py
@@ -349,19 +349,19 @@ def buf_str(buf):
     return p_ds
 
 
-def dump_buf(buf):
+def dump_buf(buf, pre=''):
     bs = buf_str(buf)
     stride = 16         # how many bytes per line
 
     # 3 chars per byte
     idx = 0
-    print('rec:  '),
+    print(pre + 'rec:  '),
     while(idx < len(bs)):
         max_loc = min(len(bs), idx + (stride * 3))
         print(bs[idx:max_loc])
         idx += (stride * 3)
         if idx < len(bs):              # if more then print counter
-            print('{:04x}: '.format(idx/3)),
+            print(pre + '{:04x}: '.format(idx/3)),
 
 
 def dt_name(rtype):
