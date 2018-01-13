7e9e126a96f8ef40d64c45bb2c22b62abd7e6bec
Eric B. Decker <cire831@gmail.com>
2018-01-12 22:32:30 -0800

    tagdump/gps_raw/geo_data: add guts of gps_geo_decoder

--- tools/utils/tagdump/tagdump/gps_decoders.py
+++ tools/utils/tagdump/tagdump/gps_decoders.py
@@ -114,10 +114,100 @@ def gps_ots_decoder(level, offset, buf, obj):
 g.mid_table[18] = (gps_ots_decoder, gps_ots_obj, "OkToSend")
 
 
+rgeo1a = '    GEO_DATA: xweek: {:4} tow: {:10}s, utc: {}/{:02}/{:02}-{:02}:{:02}:{:02}.{}'
+rgeo1b = '    lat/long: {:>16s}  {:>16s}, alt(e): {:7.2f} m  alt(msl): {:7.2f} m'
+rgeo1c = '    {:6}  {:10s}                                   {:8.2f} ft          {:8.2f} ft'
+
+rgeo2a = '    nav_valid: 0x{:04x}  nav_type: 0x{:04x}  xweek: {:4}  tow: {:10}'
+rgeo2b = '    utc: {}/{:02}/{:02}-{:02}:{:02}.{}      sat_mask: 0x{:08x}'
+rgeo2c = '    lat: {}  lon: {}  alt_elipsoid: {}  alt_msl: {}  map_datum: {}'
+rgeo2d = '    sog: {}  cog: {}  mag_var: {}  climb: {}  heading_rate: {}  ehpe: {}'
+rgeo2e = '    evpe: {}  ete: {}  ehve: {}  clock_bias: {}  clock_bias_err: {}'
+rgeo2f = '    clock_drift: {}  clock_drift_err: {}  distance: {}  distance_err: {}'
+rgeo2g = '    head_err: {}  nsats: {}  hdop: {}  additional_mode: 0x{:02x}'
+
 def gps_geo_decoder(level, offset, buf, obj):
+    obj.set(buf)
+    nav_valid   = obj['nav_valid'].val
+    nav_type    = obj['nav_type'].val
+    xweek       = obj['week_x'].val
+    tow         = obj['tow'].val
+    tow         = tow/float(1000)
+    utc_year    = obj['utc_year'].val
+    utc_month   = obj['utc_month'].val
+    utc_day     = obj['utc_day'].val
+    utc_hour    = obj['utc_hour'].val
+    utc_min     = obj['utc_min'].val
+    utc_ms      = obj['utc_ms'].val
+    utc_sec     = utc_ms/1000
+    utc_ms      = (utc_ms - utc_sec * 1000)
+    sat_mask    = obj['sat_mask'].val
+    lat         = obj['lat'].val
+    if (lat < 0):
+        lat_str = '{}'.format(-lat/float(10000000)) + '(S)'
+    else:
+        lat_str = '{}'.format(lat/float(10000000)) + '(N)'
+    lon         = obj['lon'].val
+    if (lon < 0):
+        lon_str = '{}'.format(-lon/float(10000000)) + '(W)'
+    else:
+        lon_str = '{}'.format(lon/float(10000000)) + '(E)'
+    alt_elipsoid= obj['alt_elipsoid'].val
+    alt_elipsoid /= float(100)
+    alt_msl     = obj['alt_msl'].val
+    alt_msl    /= float(100)
+    alt_e_ft    = alt_elipsoid * 3.28084
+    alt_msl_ft  = alt_msl * 3.28084
+    map_datum   = obj['map_datum'].val
+    sog         = obj['sog'].val
+    cog         = obj['cog'].val
+    mag_var     = obj['mag_var'].val
+    climb       = obj['climb'].val
+    heading_rate= obj['heading_rate'].val
+    ehpe        = obj['ehpe'].val
+    evpe        = obj['evpe'].val
+    ete         = obj['ete'].val
+    ehve        = obj['ehve'].val
+    clock_bias  = obj['clock_bias'].val
+    clock_bias_err \
+                = obj['clock_bias_err'].val
+    clock_drift = obj['clock_drift'].val
+    clock_drift_err \
+                = obj['clock_drift_err'].val
+    distance    = obj['distance'].val
+    distance_err= obj['distance_err'].val
+    head_err    = obj['head_err'].val
+    nsats       = obj['nsats'].val
+    hdop        = obj['hdop'].val
+    additional_mode \
+                = obj['additional_mode'].val
+
+    if (nav_valid & 1):
+        print(' nl'),
+        lock_str = 'nolock'
+    else:
+        print('  L'),
+        lock_str = 'lock'
+    print(' ({})'.format(nsats))
     if (level >= 1):
-        obj.set(buf)
-        print(obj)
+        print(rgeo1a.format(xweek, tow, utc_year, utc_month, utc_day,
+                            utc_hour, utc_min, utc_sec, utc_ms))
+        print(rgeo1b.format(lat_str, lon_str, alt_elipsoid, alt_msl))
+        print(rgeo1c.format(lock_str, '({} sats)'.format(nsats),
+                            alt_e_ft, alt_msl_ft))
+
+    if (level >= 2):
+        print
+        print(rgeo2a.format(nav_valid, nav_type, xweek, obj['tow'].val))
+        print(rgeo2b.format(utc_year, utc_month, utc_day, utc_hour, utc_min,
+                            obj['utc_ms'].val, sat_mask))
+        print(rgeo2c.format(lat, lon, obj['alt_elipsoid'].val,
+                            obj['alt_msl'].val, map_datum))
+        print(rgeo2d.format(sog, cog, mag_var, climb, heading_rate, ehpe))
+        print(rgeo2e.format(evpe, ete, ehve, clock_bias, clock_bias_err))
+        print(rgeo2f.format(clock_drift, clock_drift_err, distance, distance_err))
+        print(rgeo2g.format(head_err, nsats, hdop, additional_mode))
+
 
 g.mid_table[41] = (gps_geo_decoder, gps_geo_obj, "GEO_DATA")
 
