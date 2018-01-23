881e8d23bf558b158f770f2bd4e813709fcead25
Eric B. Decker <cire831@gmail.com>
2018-01-23 11:55:01 -0800

    tagdump: switch to gpl3

--- tools/utils/tagdump/tagdump/sensor_decoders.py
+++ tools/utils/tagdump/tagdump/sensor_decoders.py
@@ -1,7 +1,23 @@
-#
 # Copyright (c) 2018 Eric B. Decker
 # All rights reserved.
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# See COPYING in the top level directory of this source tree.
+#
+# Contact: Daniel J. Maltbie <dmaltbie@daloma.org>
+#          Eric B. Decker <cire831@gmail.com>
+
 # sensor data blocks decoders
 
 import globals        as     g
