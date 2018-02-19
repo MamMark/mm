23eae835c28c1714d71f302f2ddaf9f4f9dbe31e
Eric B. Decker <cire831@gmail.com>
2018-02-19 14:08:39 -0800

    convert tos/chips/gsd4e to gpl 3

--- tos/chips/gsd4e_v4/GPSMsgBufP.nc
+++ tos/chips/gsd4e_v4/GPSMsgBufP.nc
@@ -2,8 +2,26 @@
  * Copyright (c) 2017 Daniel J. Maltbie, Eric B. Decker
  * All rights reserved.
  *
- * @author Daniel J. Maltbie (dmaltbie@daloma.org)
- * @author Eric B. Decker <cire831@gmail.com>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * See COPYING in the top level directory of this source tree.
+ *
+ * Contact: Eric B. Decker <cire831@gmail.com>
+ *          Daniel J. Maltbie <dmaltbie@daloma.org>
+ */
+
+/*
+ * Buffer Slicing:
  *
  * This module handles carving a single receive buffer into logical
  * messages for the gps module to use.  A single area of memory, gps_buf,
--- tos/chips/gsd4e_v4/gps.h
+++ tos/chips/gsd4e_v4/gps.h
@@ -1,13 +1,28 @@
 /*
  * Copyright (c) 2017, Eric B. Decker, Daniel J. Maltbie
- * GPS platform defines
+ * All rights reserved.
  *
- * @author Eric B. Decker (cire831@gmail.com)
- * @author Daniel J. Maltbie (dmaltbie@daloma.org)
- * @date 27 May 2008
- * @updated Mar 2017 for UART based GSD4e chips
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * See COPYING in the top level directory of this source tree.
+ *
+ * Contact: Eric B. Decker <cire831@gmail.com>
+ *          Daniel J. Maltbie <dmaltbie@daloma.org>
  */
 
+/*
+ * GPS platform defines
+ */
 
 #ifndef __GPS_H__
 #define __GPS_H__
