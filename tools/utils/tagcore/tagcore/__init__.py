0614a2b43cd84505f865714f0c10f0d4d95b391a
Eric B. Decker <cire831@gmail.com>
2018-05-14 11:43:28 -0700

    tagcore: release 0.3.1.dev
    o core tagdump objects
    o gps_cmds
    o tagfile

--- tools/utils/tagcore/tagcore/__init__.py
+++ tools/utils/tagcore/tagcore/__init__.py
@@ -1,11 +1,9 @@
 """
-sirfdump:  decode and display sirfbin messages
+tagcore: utility and common routines for many tag things
 @author:   Eric B. Decker
 """
 
-# 0.3.0         pull core objects from tagdump into tagcore
-
-__version__ = '0.3.0.dev1'
+__version__ = '0.3.1.dev0'
 
 __all__ = [
     'CORE_REV',                         # core_rev.py
@@ -24,3 +22,7 @@ from    .core_rev       import CORE_REV
 from    .misc_utils     import buf_str, dump_buf
 from    .dt_defs        import dt_name, print_hdr, print_record
 from    .core_headers   import dt_hdr_obj
+
+# 0.3.1         pull TagFile into tagcore
+#               add gps_cmds and canned messages as gps_cmds.py
+# 0.3.0         pull core objects from tagdump into tagcore
