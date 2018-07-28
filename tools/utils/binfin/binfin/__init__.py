4443990468cfb018cfd604e94307cd57ae6ee007
Eric B. Decker <cire831@gmail.com>
2018-07-27 23:27:14 -0700

    binfin: fixed NIB vs. GOLDEN offsets.  didn't pop version
    release candidate 0.1.1rc0

--- tools/utils/binfin/binfin/__init__.py
+++ tools/utils/binfin/binfin/__init__.py
@@ -3,6 +3,8 @@ binfin: Update MamMark (mm) META_INFO data
 @author: R. Li Fo Sjoe
 """
 
-__version__ = '0.1.0.dev1'
+__version__ = '0.1.1rc0'
 
+# 0.1.1rc0      Release Candidate
+#               correctly handles NIB vs GOLDEN offsets.
 # 0.1.0.dev0    Initial Release
