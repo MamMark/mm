de5ff6b44963155713ef06f1302ecbbb90c516f3
Eric B. Decker <cire831@gmail.com>
2018-10-15 18:13:48 -0700

    gdb/timers: display other CCRs and CCTLs if being used.

--- tos/platforms/mm/gdb_hwMm.py
+++ tos/platforms/mm/gdb_hwMm.py
@@ -13,6 +13,12 @@ TA1    = TA0    + 0x400
 TA2    = TA1    + 0x400
 TA3    = TA2    + 0x400
 
+def ccr_n(ta, n):
+    return int(gdb.parse_and_eval(ta + '->CCR[{}]'.format(n)))
+
+def cctl_n(ta, n):
+    return int(gdb.parse_and_eval(ta + '->CCTL[{}]'.format(n)))
+
 class HwTMilli(gdb.Command):
     """Display h/w timer info, TMilli."""
     def __init__ (self):
@@ -21,9 +27,9 @@ class HwTMilli(gdb.Command):
     def invoke (self, args, from_tty):
         ta     = '((Timer_A_Type *) 0x{:08x})'.format(TA1)
         ctl    = int(gdb.parse_and_eval(ta + '->CTL'))
-        cctl0  = int(gdb.parse_and_eval(ta + '->CCTL[0]'))
+        cctl0  = cctl_n(ta, 0)
         r      = int(gdb.parse_and_eval(ta + '->R'))
-        ccr0   = int(gdb.parse_and_eval(ta + '->CCR[0]'))
+        ccr0   = ccr_n(ta, 0)
         upper  = int(gdb.parse_and_eval('TransformCounterC__0__m_upper'))
         lower  = r >> 5
         tmilli = (upper << 11) | (r >> 5)
@@ -34,6 +40,12 @@ class HwTMilli(gdb.Command):
 
         print('   TA1: {:04x}        CTL: {:04x}   CCTL0: {:04x}      CCR0: {:04x}'.format(
             r, ctl, cctl0, ccr0))
+        for _n in range(1, 5):
+            _ccr  = ccr_n(ta, _n)
+            _cctl = cctl_n(ta, _n)
+            if _ccr or (_cctl & 0xfffe):
+                print('{}CCTL{}: {:04x}      CCR{}: {:04x}'.format(
+                    32*' ', _n, _cctl, _n, _ccr))
         print('tmilli: {:08x}  a0_t0: {:08x}  dt: {:08x}  c0_u: {:08x}'.format(
             tmilli, a0_t0, a0_dt, c0_u))
 
