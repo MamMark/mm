7e7ffdeaffcd029940077b08be01985faf005be1
Eric B. Decker <cire831@gmail.com>
2020-01-05 04:05:52 -0800

    core/acceln: implement acceln record processing/decoding.
    - add nsample object.
    - handle acceln (raw 16 bits), acceln8 (8 bit data).

--- tools/utils/tagcore/tagcore/sensor_headers.py
+++ tools/utils/tagcore/tagcore/sensor_headers.py
@@ -21,13 +21,75 @@
 
 from   __future__   import print_function
 
-__version__ = '0.4.6.dev1'
+__version__ = '0.4.6.dev2'
 
 from   collections  import OrderedDict
 from   base_objs    import *
+
+def acceln_element():
+    return aggie(OrderedDict([
+        ('x', atom(('<H', '{}'))),
+        ('y', atom(('<H', '{}'))),
+        ('z', atom(('<H', '{}'))),
+    ]))
+
+def acceln8_element():
+    return aggie(OrderedDict([
+        ('x', atom(('B', '{}'))),
+        ('y', atom(('B', '{}'))),
+        ('z', atom(('B', '{}'))),
+    ]))
+
+def acceln8s_element():
+    return aggie(OrderedDict([
+        ('x', atom(('b', '{}'))),
+        ('y', atom(('b', '{}'))),
+        ('z', atom(('b', '{}'))),
+    ]))
+
+obj_acceln   = acceln_element()
+obj_acceln8  = acceln8_element()
+obj_acceln8s = acceln8s_element()
+
+
+def process_accel(sub_obj, decode_obj, buf):
+    for k in sub_obj.iterkeys():
+        if isinstance(k,int):
+            del sub_obj[k]
+
+    consumed = sub_obj.set(buf)
+    nsamples = sub_obj['nsamples'].val
+
+    for n in range(nsamples):
+        d = OrderedDict()
+        consumed += decode_obj.set(buf[consumed:])
+        for k, v in decode_obj.items():
+            d[k] = v.val
+        sub_obj[n] = d
+    return consumed
+
+
+def decode_acceln(level, offset, buf, obj):
+    return process_accel(obj, obj_acceln, buf)
+
+def decode_acceln8(level, offset, buf, obj):
+    return process_accel(obj, obj_acceln8, buf)
+
+def decode_acceln8s(level, offset, buf, obj):
+    return process_accel(obj, obj_acceln8s, buf)
+
 
 def obj_tmp_px():
     return aggie(OrderedDict([
         ('tmp_p', atom(('<h', '{}'))),
         ('tmp_x', atom(('<h', '{}'))),
     ]))
+
+def obj_nsample():
+    return aggie(OrderedDict([
+        ('nsamples', atom(('<H', '{}'))),
+        ('datarate', atom(('<H', '{}'))),
+    ]))
+
+#why doesnt this work?
+#obj_acceln8 = obj_nsample_hdr()
